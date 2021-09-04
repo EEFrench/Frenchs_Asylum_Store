45041309d9d2f9d6154453c24230108fdaec1ac5
EEFrench <edward.e.french@gmail.com>
2021-09-04 16:41:51 -0700

    Finished up from study group

--- f_a_store/src/App.js
+++ f_a_store/src/App.js
@@ -1,18 +1,27 @@
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import { getProducts } from './utils/store_utils';
-//import Products from './components/Products';
-
+import Products from './components/Products';
 
 function App() {
+  const [items, setItems] = useState([]);
 
-  getProducts(); //INVOKES getProducts FUNCTION
+  useEffect(() => {
+    getProducts().then((data) => setItems(data)); //INVOKES getProducts 
+    //FUNCTION, STORE THE DATA INSIDE setItems. THE ".then" IS A 
+    //SUBSTITUTE FOR ASYNC & AWAIT
+  },[]
+  )
 
   return (
     <div className="App">
-      
+      <Products
+      items ={items} // FIRST "items" IS A VARIABLE, 
+      //SECOND {items} IS THE PROP
+      />
     </div>
   );
 }
+
 
 export default App;
